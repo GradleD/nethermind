185e209e8c2c0175aaf420d825f55cbf431b38b2
Ruben Buniatyan <rubo@users.noreply.github.com>
2024-11-05 21:21:18 +0100

    test

--- src/Nethermind/Nethermind.Runner.Test/EthereumRunnerTests.cs
+++ src/Nethermind/Nethermind.Runner.Test/EthereumRunnerTests.cs
@@ -1,172 +1,172 @@
-// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
+//// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+//// SPDX-License-Identifier: LGPL-3.0-only
 
-#nullable enable
-using System;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.IO;
-using System.IO.Abstractions;
-using System.Runtime.Loader;
-using System.Threading;
-using System.Threading.Tasks;
-using Nethermind.Api;
-using Nethermind.Config;
-using Nethermind.Core.Test.IO;
-using Nethermind.Hive;
-using Nethermind.JsonRpc;
-using Nethermind.JsonRpc.Modules;
-using Nethermind.Logging;
-using Nethermind.Network.Config;
-using Nethermind.Runner.Ethereum;
-using Nethermind.Runner.Ethereum.Api;
-using NUnit.Framework;
+//#nullable enable
+//using System;
+//using System.Collections;
+//using System.Collections.Concurrent;
+//using System.IO;
+//using System.IO.Abstractions;
+//using System.Runtime.Loader;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Nethermind.Api;
+//using Nethermind.Config;
+//using Nethermind.Core.Test.IO;
+//using Nethermind.Hive;
+//using Nethermind.JsonRpc;
+//using Nethermind.JsonRpc.Modules;
+//using Nethermind.Logging;
+//using Nethermind.Network.Config;
+//using Nethermind.Runner.Ethereum;
+//using Nethermind.Runner.Ethereum.Api;
+//using NUnit.Framework;
 
-namespace Nethermind.Runner.Test;
+//namespace Nethermind.Runner.Test;
 
-[TestFixture]
-public class EthereumRunnerTests
-{
-    static EthereumRunnerTests()
-    {
-        AssemblyLoadContext.Default.Resolving += (context, name) =>
-        {
-            return null;
-        };
-    }
+//[TestFixture]
+//public class EthereumRunnerTests
+//{
+//    static EthereumRunnerTests()
+//    {
+//        AssemblyLoadContext.Default.Resolving += (context, name) =>
+//        {
+//            return null;
+//        };
+//    }
 
-    private static readonly Lazy<ICollection>? _cachedProviders = new(InitOnce);
+//    private static readonly Lazy<ICollection>? _cachedProviders = new(InitOnce);
 
-    private static ICollection InitOnce()
-    {
-        // by pre-caching configs providers we make the tests do lot less work
-        ConcurrentQueue<(string, ConfigProvider)> result = new();
-        Parallel.ForEach(Directory.GetFiles("configs"), configFile =>
-        {
-            var configProvider = new ConfigProvider();
-            configProvider.AddSource(new JsonConfigSource(configFile));
-            configProvider.Initialize();
-            result.Enqueue((configFile, configProvider));
-        });
+//    private static ICollection InitOnce()
+//    {
+//        // by pre-caching configs providers we make the tests do lot less work
+//        ConcurrentQueue<(string, ConfigProvider)> result = new();
+//        Parallel.ForEach(Directory.GetFiles("configs"), configFile =>
+//        {
+//            var configProvider = new ConfigProvider();
+//            configProvider.AddSource(new JsonConfigSource(configFile));
+//            configProvider.Initialize();
+//            result.Enqueue((configFile, configProvider));
+//        });
 
-        return result;
-    }
+//        return result;
+//    }
 
-    public static IEnumerable ChainSpecRunnerTests
-    {
-        get
-        {
-            int index = 0;
-            foreach (var cachedProvider in _cachedProviders!.Value)
-            {
-                yield return new TestCaseData(cachedProvider, index);
-                index++;
-            }
-        }
-    }
+//    public static IEnumerable ChainSpecRunnerTests
+//    {
+//        get
+//        {
+//            int index = 0;
+//            foreach (var cachedProvider in _cachedProviders!.Value)
+//            {
+//                yield return new TestCaseData(cachedProvider, index);
+//                index++;
+//            }
+//        }
+//    }
 
-    [TestCaseSource(nameof(ChainSpecRunnerTests))]
-    [MaxTime(300000)] // just to make sure we are not on infinite loop on steps because of incorrect dependencies
-    public async Task Smoke((string file, ConfigProvider configProvider) testCase, int testIndex)
-    {
-        if (testCase.configProvider is null)
-        {
-            // some weird thing, not worth investigating
-            return;
-        }
+//    [TestCaseSource(nameof(ChainSpecRunnerTests))]
+//    [MaxTime(300000)] // just to make sure we are not on infinite loop on steps because of incorrect dependencies
+//    public async Task Smoke((string file, ConfigProvider configProvider) testCase, int testIndex)
+//    {
+//        if (testCase.configProvider is null)
+//        {
+//            // some weird thing, not worth investigating
+//            return;
+//        }
 
-        await SmokeTest(testCase.configProvider, testIndex, 30330);
-    }
+//        await SmokeTest(testCase.configProvider, testIndex, 30330);
+//    }
 
-    [TestCaseSource(nameof(ChainSpecRunnerTests))]
-    [MaxTime(30000)] // just to make sure we are not on infinite loop on steps because of incorrect dependencies
-    public async Task Smoke_cancel((string file, ConfigProvider configProvider) testCase, int testIndex)
-    {
-        if (testCase.configProvider is null)
-        {
-            // some weird thing, not worth investigating
-            return;
-        }
+//    [TestCaseSource(nameof(ChainSpecRunnerTests))]
+//    [MaxTime(30000)] // just to make sure we are not on infinite loop on steps because of incorrect dependencies
+//    public async Task Smoke_cancel((string file, ConfigProvider configProvider) testCase, int testIndex)
+//    {
+//        if (testCase.configProvider is null)
+//        {
+//            // some weird thing, not worth investigating
+//            return;
+//        }
 
-        await SmokeTest(testCase.configProvider, testIndex, 30430, true);
-    }
+//        await SmokeTest(testCase.configProvider, testIndex, 30430, true);
+//    }
 
-    private static async Task SmokeTest(ConfigProvider configProvider, int testIndex, int basePort, bool cancel = false)
-    {
-        // An ugly hack to keep unused types
-        Console.WriteLine(typeof(IHiveConfig));
+//    private static async Task SmokeTest(ConfigProvider configProvider, int testIndex, int basePort, bool cancel = false)
+//    {
+//        // An ugly hack to keep unused types
+//        Console.WriteLine(typeof(IHiveConfig));
 
-        var tempPath = TempPath.GetTempDirectory();
-        Directory.CreateDirectory(tempPath.Path);
+//        var tempPath = TempPath.GetTempDirectory();
+//        Directory.CreateDirectory(tempPath.Path);
 
-        Exception? exception = null;
-        try
-        {
-            IInitConfig initConfig = configProvider.GetConfig<IInitConfig>();
-            initConfig.BaseDbPath = tempPath.Path;
+//        Exception? exception = null;
+//        try
+//        {
+//            IInitConfig initConfig = configProvider.GetConfig<IInitConfig>();
+//            initConfig.BaseDbPath = tempPath.Path;
 
-            INetworkConfig networkConfig = configProvider.GetConfig<INetworkConfig>();
-            int port = basePort + testIndex;
-            networkConfig.P2PPort = port;
-            networkConfig.DiscoveryPort = port;
+//            INetworkConfig networkConfig = configProvider.GetConfig<INetworkConfig>();
+//            int port = basePort + testIndex;
+//            networkConfig.P2PPort = port;
+//            networkConfig.DiscoveryPort = port;
 
-            INethermindApi nethermindApi = new ApiBuilder(configProvider, LimboLogs.Instance).Create();
-            nethermindApi.RpcModuleProvider = new RpcModuleProvider(new FileSystem(), new JsonRpcConfig(), LimboLogs.Instance);
-            EthereumRunner runner = new(nethermindApi);
+//            INethermindApi nethermindApi = new ApiBuilder(configProvider, LimboLogs.Instance).Create();
+//            nethermindApi.RpcModuleProvider = new RpcModuleProvider(new FileSystem(), new JsonRpcConfig(), LimboLogs.Instance);
+//            EthereumRunner runner = new(nethermindApi);
 
-            using CancellationTokenSource cts = new();
+//            using CancellationTokenSource cts = new();
 
-            try
-            {
-                Task task = runner.Start(cts.Token);
-                if (cancel)
-                {
-                    cts.Cancel();
-                }
+//            try
+//            {
+//                Task task = runner.Start(cts.Token);
+//                if (cancel)
+//                {
+//                    cts.Cancel();
+//                }
 
-                await task;
-            }
-            catch (Exception e)
-            {
-                exception = e;
-            }
-            finally
-            {
-                try
-                {
-                    await runner.StopAsync();
-                }
-                catch (Exception e)
-                {
-                    if (exception is not null)
-                    {
-                        await TestContext.Error.WriteLineAsync(e.ToString());
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-            }
-        }
-        finally
-        {
-            try
-            {
-                tempPath.Dispose();
-            }
-            catch
-            {
-                if (exception is not null)
-                {
-                    // just swallow this exception as otherwise this is recognized as a pattern byt GitHub
-                    // await TestContext.Error.WriteLineAsync(e.ToString());
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
-    }
-}
+//                await task;
+//            }
+//            catch (Exception e)
+//            {
+//                exception = e;
+//            }
+//            finally
+//            {
+//                try
+//                {
+//                    await runner.StopAsync();
+//                }
+//                catch (Exception e)
+//                {
+//                    if (exception is not null)
+//                    {
+//                        await TestContext.Error.WriteLineAsync(e.ToString());
+//                    }
+//                    else
+//                    {
+//                        throw;
+//                    }
+//                }
+//            }
+//        }
+//        finally
+//        {
+//            try
+//            {
+//                tempPath.Dispose();
+//            }
+//            catch
+//            {
+//                if (exception is not null)
+//                {
+//                    // just swallow this exception as otherwise this is recognized as a pattern byt GitHub
+//                    // await TestContext.Error.WriteLineAsync(e.ToString());
+//                }
+//                else
+//                {
+//                    throw;
+//                }
+//            }
+//        }
+//    }
+//}
